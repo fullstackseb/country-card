32e7cb56c66bc34685a6bf3a490b28db0d4bc27c
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2022-09-24 11:10:28 +0200

    made some css changes and implemented the JS pseudo logic

---
+++ README.md
@@ -0,0 +1,5 @@
+# Country card
+
+A simple application which uses the [REST Country API]('https://restcountries.com/') to get data about countries worldwide. The user can simply input a country of choice and display a card stating some facts about this country.
+
+This app is hosted on Github pages and can be [found here]('https://github.io/fullstackseb/country-card').
--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 'use strict'
 
-const btn = document.querySelector('.btn-country')
-const countriesContainer = document.querySelector('.countries')
+const btnSearch = document.querySelector('.btn-search')
+const countryCard = document.querySelector('.country-card')
 const countryInput = document.querySelector('.search')
 
-console.log(countryInput.innerText)
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
 
-///////////////////////////////////////
-const cardHTML = `
-      <div class="countries">
-        <article class="country">
-          <img class="country__img" src="" />
-          <div class="country__data">
-            <h3 class="country__name">COUNTRY</h3>
-            <h4 class="country__region">REGION</h4>
-            <p class="country__row"><span>👫</span>POP people</p>
-            <p class="country__row"><span>🗣️</span>LANG</p>
-            <p class="country__row"><span>💰</span>CUR</p>
-          </div>
-        </article>
-      </div>
-`
+btnSearch.addEventListener('click', e => {
+  console.log(countryInput.value)
+  // take input as API search parameter
+  checkInputString()
+
+  // call api
+  getCountryData()
+
+  // put data into cardHTML
+  // buildCountryCard(data)
+
+  insertCardData(cardHTML)
+
+  countryInput.value = ''
+})
+
+const getCountryData = async countryName => {
+  // built search param
+  // fecth data from api
+  // destructure data and return it
+}
+
+const checkInputString = countryName => {
+  // check string is not empty
+  // convert to lower case
+  // return string
+}
+
+/* const buildCountryCard = (data) => {
+  const cardHTML =  `
+  <article class="country">
+    <img class="country__img" src="${}" />
+    <div class="country__data">
+      <h3 class="country__name">COUNTRY${}</h3>
+      <h4 class="country__region">REGION${}</h4>
+      <p class="country__row"><span>👫</span>POP people${}</p>
+      <p class="country__row"><span>🗣️</span>LANG${}</p>
+      <p class="country__row"><span>💰</span>CUR${}</p>
+    </div>
+  </article>
+  `
+  return cardHTML
+} */
+
+const insertCardData = cardHTML => {
+  countryCard.innerHTML = cardHTML
+  countryCard.style.opacity = 1
+}
